538f38a8ff1d9b1460e2cbf37a05f29befa3705b
BuildTools <unconfigured@null.spigotmc.org>
2023-01-16 11:22:35 -0800

    Fixing reference changes for 1.19.3

--- src/main/java/io/sandbox/lib/BlockHelper.java
+++ src/main/java/io/sandbox/lib/BlockHelper.java
@@ -3,13 +3,13 @@ package io.sandbox.lib;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.registry.Registries;
 
 public class BlockHelper {
   public static Block getBlockFromId(String blockId) {
     if (blockId == null) { return null; }
 
-    Block foundBlock = Registry.BLOCK.get(new Identifier(blockId));
+    Block foundBlock = Registries.BLOCK.get(new Identifier(blockId));
 
     if (foundBlock == Blocks.AIR && blockId != "minecraft:air") { return null; }
 
